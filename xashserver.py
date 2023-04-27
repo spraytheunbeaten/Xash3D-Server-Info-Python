edd15d58db50e3a829d4ae1c52937457febb2ce4
SpRaY <102756531+SpRaY1337@users.noreply.github.com>
2023-04-27 19:20:38 +0300

    Updated
    Added categorys and port input

--- xashserver.py
+++ xashserver.py
@@ -1,36 +1,110 @@
-#import
-
 import socket
+
+import struct
+
 import os
 
-#os
+os.system('clear')
 
-os.system("clear")
+print("--------------------Xash3D Server Info--------------------")
 
-#input
+ip = input("IP Adress: ")
 
-print("----- Coded by SpRaY ----- \n")
-print("Discord: ! SpRaY \n")
-
-HOST = input("Sunucu IP adresi: ")
-PORT = 27015
-
-#socket
+port = int(input("Port: "))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-message = b"\xff\xff\xff\xffTSource"
-s.sendto(message, (HOST, PORT))
+s.settimeout(3.0)
 
-#get info
+s.connect((ip, port))
+
+packet = b"\xFF\xFF\xFF\xFFTSource Engine Query\x00"
+
+s.send(packet)
 
 try:
-    data, addr = s.recvfrom(1024)
-except KeyboardInterrupt:
-    print("Program sonlandırıldı.")
-else:
-    print(data)
 
-#end
+    data, addr = s.recvfrom(4096)
+
+except socket.timeout:
+
+    print("Server did'nt response!")
+
+    exit()
+
+ #color by dumb3x
+
+def remove_color_tags(text):
+
+    colors = [
+
+        "^0",
+
+        "^1",
+
+        "^2",
+
+        "^3",
+
+        "^4",
+
+        "^5",
+
+        "^6",
+
+        "^7",            
+
+        "^8",
+
+        "^9"
+
+    ] 
+
+    
+
+    for x in colors:
+
+        text = text.replace(x, "")
+
+    return text
+
+header = struct.unpack("<4s", data[:4])[0]
+
+if header == b"\xFF\xFF\xFF\xFF":
+
+    data = data[4:]
+
+    data_split = data.split(b"\x00")
+
+    server_name = data_split[0].decode("utf-8")
+
+    map_name = data_split[1].decode("utf-8")
+
+    game_dir = data_split[2].decode("utf-8")
+
+    game_desc = data_split[3].decode("utf-8")
+
+    num_players = data[49]
+
+    max_players = data[50]
+
+    print("--------------------")
+
+    print("Server Name: ", remove_color_tags(server_name))
+
+    print("--------------------")
+
+    print("Map: ", map_name)
+
+    print("--------------------")
+
+    print("Player Number: ", num_players, "/", max_players)
+
+    print("--------------------")
+
+else:
+
+    print("ERROR!")
 
 s.close()
+
